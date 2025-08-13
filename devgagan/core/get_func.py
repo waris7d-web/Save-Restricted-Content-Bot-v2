2af71227eb9824d4fd71dc7c54cc925311880f41
Gagan <business@devgagan.in>
2025-08-13 07:13:36 +0530

    Class method updated ✅

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -1,3 +1,17 @@
+# ---------------------------------------------------
+# File Name: get_func.py
+# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
+#              and uploading them back to Telegram.
+# Author: Gagan
+# GitHub: https://github.com/devgaganin/
+# Telegram: https://t.me/team_spy_pro
+# YouTube: https://youtube.com/@dev_gagan
+# Created: 2025-01-11
+# Last Modified: 2025-01-11
+# Version: 2.0.5
+# License: MIT License
+# ---------------------------------------------------
+
 import asyncio
 import os
 import re
